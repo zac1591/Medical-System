69e7fd34b0f76f91a08809c32286b29066f83bb1
Isaac Abrego <abregoisaac01@gmail.com>
2020-10-09 11:41:19 -0600

    React boiler plate for medical system

---
+++ src/components/menu-principal-izquierdo/menu-principal.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import './menu-principal-izquierdo.scss';
+
+function MenuPrincipal() {
+    return (
+        <div className="contenedor-principal-flex">
+            <h1>Menu principal</h1>
+        </div>
+    )
+}
+
+export default MenuPrincipal;
---
+++ src/pages/contenedor.principal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MenuPrincipal from '../components/menu-principal-izquierdo/menu-principal.component'
+
+function ContenedorPrincipal() {
+    return (
+        <div className="contenedor-flexible">
+            <MenuPrincipal />
+        </div>
+    )
+}
+
+export default ContenedorPrincipal;
